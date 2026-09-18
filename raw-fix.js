d0c5fcdcdd81e9c0a34277260b916f6fe9a8e6bd
SamStace <samstace@gmail.com>
2026-09-18 16:21:17 +0530

    Point image fallback at repaired assets

--- raw-fix.js
+++ raw-fix.js
@@ -1,3 +1,2 @@
-const rawAssets='https://raw.githubusercontent.com/SamStace/workaway-portfolio/1fe6d2a35d7d0920948e4504307508fb87765034/site-assets/';
+const rawAssets='https://raw.githubusercontent.com/SamStace/workaway-portfolio/1a00be9508b4075f0ee0b7fd8bf0a7ddc1c2d0c6/site-assets/';
 document.querySelectorAll('img[src^="site-assets/"]').forEach(img=>{const relative=img.getAttribute('src').replace(/^site-assets\//,''); const absolute=rawAssets+relative; img.src=absolute; img.closest('[data-src]')?.setAttribute('data-src',absolute)});
-
