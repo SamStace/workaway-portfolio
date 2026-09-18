473e361a457d44d6db8282c0861f71ca70ec467f
SamStace <samstace@gmail.com>
2026-09-18 16:16:33 +0530

    Pin image fallback assets to valid commit

--- raw-fix.js
+++ raw-fix.js
@@ -1,3 +1,3 @@
-const rawAssets='https://raw.githubusercontent.com/SamStace/workaway-portfolio/main/site-assets/';
+const rawAssets='https://raw.githubusercontent.com/SamStace/workaway-portfolio/1fe6d2a35d7d0920948e4504307508fb87765034/site-assets/';
 document.querySelectorAll('img[src^="site-assets/"]').forEach(img=>{const relative=img.getAttribute('src').replace(/^site-assets\//,''); const absolute=rawAssets+relative; img.src=absolute; img.closest('[data-src]')?.setAttribute('data-src',absolute)});
 
